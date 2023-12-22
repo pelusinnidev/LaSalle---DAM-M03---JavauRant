eb9161120051f6e931904198f69b6be6b46efb27
Pelusinni <110785576+Pelusinni@users.noreply.github.com>
2023-12-22 12:54:05 +0100

    Update Restaurant.java

--- src/Restaurant.java
+++ src/Restaurant.java
@@ -3,7 +3,8 @@ import java.util.Scanner;
 public class Restaurant {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        int opcio;
+        int opcioprincipal = 1;
+        int opciocarta = 1;
         do {
             System.out.println("** Menú **");
             System.out.println("1. Veure menú");
@@ -12,17 +13,37 @@ public class Restaurant {
             System.out.println("4. Sortir");
 
             // Llegeix l'opció seleccionada per l'usuari
-            opcio = input.nextInt();
+            opcioprincipal = input.nextInt();
 
             // Executa la funcionalitat corresponent a l'opció seleccionada
-            switch (opcio) {
+            switch (opcioprincipal) {
                 case 1:
-
+                    //Opció per mostrar el menú
+                    System.out.println("** Menú **");
+                    System.out.println("** 1. Amanides **");
+                    System.out.println("** 2. Pastes **");
+                    System.out.println("** 3. Carns **");
+                    System.out.println("** 4. Peixos **");
+                    System.out.println("** 6. Begudes Alcoholiques **");
+                    System.out.println("** 7. Begudes no Alcoholiques **");
+                    System.out.println("");
+                    System.out.println("Introdueix un nombre del 1 al 7 per escollir...");
+                    opciocarta = input.nextInt();
+                    switch (opciocarta) {
+                        case 1:
+                            System.out.println("** Amanides **");
+                        break;
+                        case 2:
+                            System.out.println("** Pastes **");
+                            break;
+                    }
                     break;
                 case 2:
+                    //Opció per mostrar la comanda
 
                     break;
                 case 3:
+                    //Opció per imprimir la factura
 
                     break;
                 case 4:
